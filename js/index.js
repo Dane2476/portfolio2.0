66603eaa0c0a12b77a61efad93500606ecee5c9b
Dane Maison <Danemaison@gmail.com>
2019-06-13 00:00:35 -0700

    Added image caption and project header caption. Fixed mobile x-overflow.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
     item.classList.add('loaded');
     item.classList.remove('preload');
   });
+
   var toTop = document.querySelector("#to-top");
   document.addEventListener('scroll', function(){
     if (window.innerWidth > 799.88){
@@ -20,12 +21,18 @@ document.addEventListener("DOMContentLoaded", function(){
     else{
       return false;
     }
-
   });
-
   document.querySelector("#to-top").addEventListener("click", function(){
     window.scrollTo({top:0,behavior:'smooth'}); // Not supported on Safari or IE
   });
+
+  document.querySelector('.self-portrait').addEventListener("mouseover", function(){
+    document.querySelector('.caption').style.opacity = '1';
+  });
+  document.querySelector('.self-portrait').addEventListener("mouseout", function(){
+    document.querySelector('.caption').style.opacity = '0';
+  });
+
 });
 
 function colorDetect(){
